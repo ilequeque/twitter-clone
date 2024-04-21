7e9e621cb102e693ab1c87f49d2b1f409defed2e
ilequeque <ilyas.shakhabdinov@gmail.com>
2024-04-22 03:19:45 +0500

    Initial Commit

---
+++ Twitter-clone/Twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_cloneApp.swift
+//  Twitter-clone
+//
+//  Created by Iliyas Shakhabdin on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
